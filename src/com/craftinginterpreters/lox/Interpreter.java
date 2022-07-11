cc9a9c4320c6b32d79b45301fb3de264c677d631
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2022-07-11 09:41:09 +0200

    Chapter 8 - Statements

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,9 @@
 package com.craftinginterpreters.lox;
 
-public class Interpreter implements Expr.Visitor<Object>{
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>,
+                                    Stmt.Visitor<Void>{
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object right = evaluate(expr.right);
@@ -102,13 +105,18 @@ public class Interpreter implements Expr.Visitor<Object>{
         throw new RuntimeError(operator, "Operands must be a numbers.");
     }
 
-    void interpret(Expr expression) {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
     }
 
     private String stringify(Object object) {
@@ -123,5 +131,18 @@ public class Interpreter implements Expr.Visitor<Object>{
         }
 
         return object.toString();
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 }
