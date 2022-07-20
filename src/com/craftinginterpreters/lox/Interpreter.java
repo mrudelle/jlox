9ea4818e11d2ff220a747e6c343cbecf738c243e
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2022-07-20 22:39:42 +0200

    Chapter 10 - Function call

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,10 +1,52 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>,
                                     Stmt.Visitor<Void>{
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis()/1000.0;
+            }
+
+            @Override
+            public int arity() { return 0; }
+        });
+    }
+
+    void interpret(List<Stmt> statements) {
+        try {
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    void executeBlock(List<Stmt> statements, Environment environment) {
+        Environment previous = this.environment;
+
+        try {
+            this.environment = environment;
+
+            for (Stmt statement: statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
 
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
@@ -67,10 +109,26 @@ public class Interpreter implements Expr.Visitor<Object>,
         };
     }
 
-    private boolean isEqual(Object a, Object b) {
-        if (a == null && b == null) return true;
-        if (a == null) return false;
-        return a.equals(b);
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " + function.arity() + " arguments but got " + arguments.size() + ".");
+        }
+
+        return function.call(this, arguments);
     }
 
     @Override
@@ -112,6 +170,12 @@ public class Interpreter implements Expr.Visitor<Object>,
         return environment.get(expr.name);
     }
 
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
+
     private boolean isTruthy(Object object) {
         if (object == null) return false;
         if (object instanceof Boolean) return (boolean)object;
@@ -132,20 +196,6 @@ public class Interpreter implements Expr.Visitor<Object>,
         throw new RuntimeError(operator, "Operands must be a numbers.");
     }
 
-    void interpret(List<Stmt> statements) {
-        try {
-            for (Stmt statement : statements) {
-                execute(statement);
-            }
-        } catch (RuntimeError error) {
-            Lox.runtimeError(error);
-        }
-    }
-
-    private void execute(Stmt stmt) {
-        stmt.accept(this);
-    }
-
     private String stringify(Object object) {
         if (object == null) return "nil";
 
@@ -164,20 +214,6 @@ public class Interpreter implements Expr.Visitor<Object>,
     public Void visitBlockStmt(Stmt.Block stmt) {
         executeBlock(stmt.statements, new Environment(environment));
         return null;
-    }
-
-    void executeBlock(List<Stmt> statements, Environment environment) {
-        Environment previous = this.environment;
-
-        try {
-            this.environment = environment;
-
-            for (Stmt statement: statements) {
-                execute(statement);
-            }
-        } finally {
-            this.environment = previous;
-        }
     }
 
     @Override
